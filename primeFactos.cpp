7be7a01145b46b81dc68ab6af71726d6f72cc1ed
rootless <ripon1995dc@gmail.com>
2021-07-17 21:45:50 +0600

    Add files via upload

---
+++ primeFactos.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<cmath>
+using namespace std;
+
+void primeFactors(int n)
+{
+    printf("Prime factors are: ");
+    while(n%2==0)
+    {
+        printf("2 ");
+        n/=2;
+    }
+
+    for(int i=3;i<sqrt(n);i+=2)
+    {
+        if(n%i == 0)
+        {
+            printf("%d ", i);
+            n/=i;
+        }
+    }
+    if(n>2)
+    {
+        printf("%d",n);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    primeFactors(n);
+    main();
+    return 0;
+}
+
