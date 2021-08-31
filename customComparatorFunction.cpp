1310feb75ba39a0d010762cf19c5a3587c4305d7
rootless <ripon1995dc@gmail.com>
2021-09-01 01:57:07 +0600

    Add files via upload

---
+++ customComparatorFunction.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct Point
+{
+    int x,y;
+};
+
+bool cmp(Point A, Point B)
+{
+    if(A.x<B.x)return true;
+    if(A.x>B.x)return false;
+
+    if(A.y<B.y)return true;
+    if(A.y>B.y)return false;
+
+    return false;
+}
+
+int main()
+{
+    Point p[4];
+
+    p[0].x=100;p[0].y=20;
+
+    p[1].x=15;p[1].y=30;
+
+    p[2].x=15;p[2].y=40;
+
+    p[3].x=2;p[3].y=30;
+
+    for(int i=0;i<4;i++)
+    {
+        cout<<p[i].x<<" "<<p[i].y<<endl;
+    }
+
+
+    sort(p,p+4,cmp);
+
+    cout<<"After sorting: "<<endl;
+    for(int i=0;i<4;i++)
+    {
+        cout<<p[i].x<<" "<<p[i].y<<endl;
+    }
+
+    return 0;
+}
+
